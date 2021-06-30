f0fea57e79df4d9a5ae67f21428f76f65dd3c067
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 23:06:37 +0700

    js

---
+++ tugas27.js
@@ -0,0 +1,9 @@
+function gabungkan(){
+    var str1 = "Nomor ", str2 = "rumah saya ", str3 = 30, str4= " A";
+    console.log(str1.concat(str2, str3, str4));
+
+    var str5 = "Jalan pangeran Tirtayasa No ", str6 = 70;
+    console.log(str5.concat(str6));
+}
+
+gabungkan();
